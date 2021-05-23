7f880c031dddf4fbbc156a4d7f9fceb4c5624ad8
Laura <celisl2@mailbox.winthrop.edu>
2021-05-23 19:10:36 -0400

    finished most of project

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const SideBar = () => {
         <div className="header">
             <h1><span className="highlighted">Voter Suppression U.S. Bill Tracker</span></h1>
             <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu cursus risus. Curabitur eget lectus in nisl imperdiet tempor eu vitae enim. Quisque in hendrerit mauris. Cras feugiat risus justo, ac consequat sapien ultricies a. Etiam tincidunt elit arcu, eget semper augue ornare et. Mauris ultrices neque eget nulla lobortis egestas. Sed mollis non ante sed convallis.</p>
+                <p>After the 2021 election the United States has seen an increase in voter suppresion laws introduced by both The House of Representatives and the US Senate. With this in mind, we want to inform U.S. voters about these bills and their status under current laws. The data below is gathered from the Brennan Center for Justice which is an organization focused on civil rights' issues.</p>
             </div>
         </div>
     );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+let BILLS_ENDPOINT = "http://localhost:3030/bills/";
 
 const SideBar = ({currentState}) => {
-
+    const [allData, setAllData] = useState(null);
+    const [state, setState] = useState(null);
+    
     useEffect(() => {
-        
+        if(currentState){
+            BILLS_ENDPOINT += "WY";
+            console.log("About to get bills");
+            axios.get(BILLS_ENDPOINT)
+            .then((res) => {
+                console.log(res);
+                setAllData(res);
+            }).catch((err) => {
+                console.error(err);
+            });
+        }
         console.log("### " + currentState)
-    }, [currentState]);
+    }, [currentState, allData]);
 
-    return (
-        <div>
-            <h4>Current State: {currentState}</h4>
+    if(allData != null){
+        return (
+            <div className="infoBar">
+                <div className="bill-head">
+                    <h4>Showing Bills For</h4>
+                    <h4 className="currState">{currentState}</h4>
+                </div>
+                <div className="bill-body">
+                    {
+                        allData.data.suppression.map((bill, index) => {
+                            {console.log("&&&& " + JSON.stringify(bill))}
+                            <div>
+                                <h6>{bill.bill_id}</h6>
+                                <p>{bill.subject}</p>
+                            </div>
+                        })
+                    }
+                </div>
+                
+            </div>
+        )
+    } 
+    else {
+        return(
+            <div className="infoBar">
+             <div className="bill-head">
+                <h4>No Bills Found For</h4>
+                <h4 className="currState">{currentState}</h4>
+            </div>
         </div>
-    );
+        )
+        
+    }
 };
 
 export default SideBar;
